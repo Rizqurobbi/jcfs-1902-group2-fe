eb839d246f024554169378f16aca29acb3198f35
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2022-05-09 10:01:25 +0700

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://localhost:2002";
+// export const API_URL = "https://localhost:2002";
 
-// export const API_URL = "https://jcfs190202api.purwadhikabootcamp.com";
+export const API_URL = "https://jcfs190202api.purwadhikabootcamp.com";
